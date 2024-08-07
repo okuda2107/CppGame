dfa6aab02bc51bc962a95b24ae950f5b4955566c
okuda <okuda2107@gmail.com>
2024-08-07 22:23:25 +0900

    追加

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -10,6 +10,48 @@ bool Sphere::Contains(const Vector3& point) const {
 	return distSq <= (mRadius * mRadius);
 }
 
+void AABB::UpdateMinMax(const Vector3& point)
+{
+	// Update each component separately
+	mMin.x = Math::Min(mMin.x, point.x);
+	mMin.y = Math::Min(mMin.y, point.y);
+	mMin.z = Math::Min(mMin.z, point.z);
+
+	mMax.x = Math::Max(mMax.x, point.x);
+	mMax.y = Math::Max(mMax.y, point.y);
+	mMax.z = Math::Max(mMax.z, point.z);
+}
+
+void AABB::Rotate(const Quaternion& q)
+{
+	// Construct the 8 points for the corners of the box
+	std::array<Vector3, 8> points;
+	// Min point is always a corner
+	points[0] = mMin;
+	// Permutations with 2 min and 1 max
+	points[1] = Vector3(mMax.x, mMin.y, mMin.z);
+	points[2] = Vector3(mMin.x, mMax.y, mMin.z);
+	points[3] = Vector3(mMin.x, mMin.y, mMax.z);
+	// Permutations with 2 max and 1 min
+	points[4] = Vector3(mMin.x, mMax.y, mMax.z);
+	points[5] = Vector3(mMax.x, mMin.y, mMax.z);
+	points[6] = Vector3(mMax.x, mMax.y, mMin.z);
+	// Max point corner
+	points[7] = Vector3(mMax);
+
+	// Rotate first point
+	Vector3 p = Vector3::Transform(points[0], q);
+	// Reset min/max to first point rotated
+	mMin = p;
+	mMax = p;
+	// Update min/max based on remaining points, rotated
+	for (size_t i = 1; i < points.size(); i++)
+	{
+		p = Vector3::Transform(points[i], q);
+		UpdateMinMax(p);
+	}
+}
+
 bool AABB:Contains(const Vector3& point) const {
     bool outside = point.x < mMin.x ||
 		point.y < mMin.y ||
