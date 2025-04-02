a5e9ee097db23246b3d3a3336f8cc5adc65a0ebe
okuda <okuda2107@gmail.com>
2025-04-02 12:29:26 +0000

    wip

--- inc/AudioSystem.h
+++ inc/AudioSystem.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <unordered_map>
 
 class AudioSystem {
     public:
@@ -7,9 +8,18 @@ class AudioSystem {
 
     bool Initialize();
     void Shutdown();
+
+    void UnloadData();
+
     void Update(float deltaTime);
+
+    void AddAudio(class AudioComponent* audio);
+    void RemoveAudio(class AudioComponent* audio);
+
     void test();
 
     private:
     class Game* mGame;
+
+    std::unordered_map<std::string, class Sound*> mSounds;
 };
