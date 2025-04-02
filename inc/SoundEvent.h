de49225e6cf28c92232c47fcb739b2e66e79b8b1
okuda <okuda2107@gmail.com>
2025-04-02 12:29:26 +0000

    wip

--- inc/SoundEvent.h
+++ inc/SoundEvent.h
@@ -1,12 +1,33 @@
-// #pragma once
+#pragma once
+#include <string>
+#include <AL/al.h>
+#include <AL/alc.h>
 
-// class SoundEvent {
-//     public:
-//     SoundEvent();
+class SoundEvent {
+public:
+    SoundEvent();
+    ~SoundEvent();
 
-//     bool IsValid();
+    // Load sound data from file
+    bool Load(const std::string& filePath);
+    void Unload();
 
-//     private:
-//     class AudioSystem* mSystem;
-//     unsigned int mID;
-// };
+    // Returns true if associated FMOD event still exists
+    bool IsValid();
+    void Play();
+    void Stop();
+    void Pause();
+
+    // Set the volume (0.0f to 1.0f)
+    void SetVolume(float volume);
+
+    bool IsPlaying() const;
+
+private:
+    // OpenAL buffer and source
+    ALuint buffer;
+    ALuint source;
+
+    // Helper function to check for OpenAL errors
+    bool checkALError(const std::string& errorMessage) const;
+};
