a46726a2323c2543845242dabf753deafc649021
okuda <okuda2107@gmail.com>
2025-08-07 17:21:21 +0000

    linux環境cmake

--- cmakes/x64-linux.cmake
+++ cmakes/x64-linux.cmake
@@ -1,4 +1,23 @@
-include_directories("/usr/include/SDL2")
-include_directories("/usr/include/SOIL")
-include_directories("/usr/include/GL")
-include_directories("/usr/include/rapidjson")
+# set(CMAKE_INCLUDE_PATH
+#     "${CMAKE_SOURCE_DIR}/exlibs/SDL2-2.32.0/x86_64-w64-mingw32/include/SDL2"
+#     "${CMAKE_SOURCE_DIR}/exlibs/SDL2_image-2.8.5/x86_64-w64-mingw32/include/SDL2"
+#     "${CMAKE_SOURCE_DIR}/exlibs/glew-2.2.0/include/GL"
+#     "/ust/include/GL"
+#     "${CMAKE_SOURCE_DIR}/exlibs/rapidjson-1.1.0/include/rapidjson"
+#     "${CMAKE_SOURCE_DIR}/exlibs/Simple-OpenGL-Image-Library/src"
+# )
+
+# set(CMAKE_LIBRARY_PATH
+#     "${CMAKE_SOURCE_DIR}/exlibs/SDL2-2.32.0/x86_64-w64-mingw32/lib"
+#     "${CMAKE_SOURCE_DIR}/exlibs/SDL2_image-2.8.5/x86_64-w64-mingw32/lib"
+#     "${CMAKE_SOURCE_DIR}/exlibs/glew-2.2.0/build/lib"
+#     "/ust/include/GL"
+#     "${CMAKE_SOURCE_DIR}/exlibs/Simple-OpenGL-Image-Library/lib"
+# )
+
+set(CMAKE_INCLUDE_PATH
+    "/usr/include/SDL2"
+    "/usr/include/SOIL"
+    "/usr/include/GL"
+    "/usr/include/rapidjson"
+)
