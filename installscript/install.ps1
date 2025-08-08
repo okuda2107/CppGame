0daf4cd3431f1a3e15e6878a8f3ac77d93240244
okuda <okuda2107@gmail.com>
2025-08-08 12:30:53 +0900

    test済み

--- installscript/install.ps1
+++ installscript/install.ps1
@@ -36,40 +36,40 @@ if (-not (winget list --id Microsoft.VisualStudio.2022.Community | Select-String
     winget install --id Microsoft.VisualStudio.2022.Community -e --override "--quiet --add Microsoft.VisualStudio.Workload.NativeDesktop"
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "SDL2-2.32.0")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "SDL2-2.32.8"))) {
     Download-Lib "https://github.com/libsdl-org/SDL/releases/download/release-2.32.8/SDL2-devel-2.32.8-VC.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "SDL2_image-2.8.8")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "SDL2_image-2.8.8"))) {
     Download-Lib "https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.8/SDL2_image-devel-2.8.8-VC.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "SDL2_mixer-2.8.1")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "SDL2_mixer-2.8.1"))) {
     Download-Lib "https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.8.1/SDL2_mixer-devel-2.8.1-VC.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "SDL2_net-2.2.0")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "SDL2_net-2.2.0"))) {
     Download-Lib "https://github.com/libsdl-org/SDL_net/releases/download/release-2.2.0/SDL2_net-devel-2.2.0-VC.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "SDL2_ttf-2.24.0")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "SDL2_ttf-2.24.0"))) {
     Download-Lib "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.24.0/SDL2_ttf-devel-2.24.0-VC.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "rapidjson-1.1.0")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "rapidjson-1.1.0"))) {
     Download-Lib "https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.zip" $ExlibsDir
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "glew-2.2.0")) -eq $false) {
+if( -not (Test-Path (Join-Path $ExlibsDir "glew-2.2.0"))) {
     Download-Lib "https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.zip" $ExlibsDir
     $TmpDir = $(Join-Path $ExlibsDir "glew-2.2.0")
     cmake -S $(Join-Path $TmpDir "build/cmake")-B $(Join-Path $TmpDir "build")
     cmake --build $(Join-Path $TmpDir "build")
 }
 
-if(-not (Test-Path $(Join-Path $ExlibsDir "Simple-OpenGL-Image-Library")) -eq $false) {
-    git clone https://github.com/kbranigan/Simple-OpenGL-Image-Library.git $ExlibsDir
+if( -not (Test-Path (Join-Path $ExlibsDir "Simple-OpenGL-Image-Library"))) {
     $TmpDir = $(Join-Path $ExlibsDir "Simple-OpenGL-Image-Library")
-    cmake -S $TmpDir-B $(Join-Path $TmpDir "build")
+    git clone https://github.com/kbranigan/Simple-OpenGL-Image-Library.git $TmpDir
+    cmake -S $TmpDir -B $(Join-Path $TmpDir "build")
     cmake --build $(Join-Path $TmpDir "build")
 }
