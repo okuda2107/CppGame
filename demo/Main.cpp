2c43a142421b19376ce90da872c7b944d0a8dca0
okuda <okuda2107@gmail.com>
2025-11-30 01:57:08 +0900

    wip: Cmakeの環境構築

--- demo/Main.cpp
+++ demo/Main.cpp
@@ -1,6 +1,10 @@
 #include "Game.h"
+#include "temp.h"
+#include "test/temp.h"
 
 int main(int argc, char** argv) {
+    hello();
+    hello2();
     Game game;
     bool success = game.Initialize();
     if (success) {
---
+++ engine/src/core/temp.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void hello() { std::cout << "hello" << std::endl; }
+
+void hello2() { std::cout << "hello2" << std::endl; }
