e1021b5f9a67ed7401be4e35ade5deb40eaeafba
okuda <okuda2107@gmail.com>
2025-12-28 01:56:29 +0900

    wip: Sceneのみがsystem群に参照することを許可

---
+++ engine/include/game/scene/Scene.h
@@ -0,0 +1,28 @@
+#pragma once
+
+struct SceneCreateDeps {
+    /*
+        Sceneの生成ではactorの存在が前提となっているため，
+        ObjectsSystemBaseではなく，
+        ActorsSystemの方を依存関係に含める
+    */
+    class ActorsSystem& actorsSystem;
+    class RenderDB& renderDB;
+    class AudioSystem& audioSystem;
+    // class PhysicsSystem& physicsSystem;
+    class UISystem& uiSystem;
+};
+
+class Scene {
+   protected:
+    class RenderDB& mRenderDB;
+    class AudioSystem& mAudioSystem;
+    // class PhysicsSystem& mPhysicsSystem;
+    class UISystem& mUISystem;
+
+   public:
+    Scene(struct SceneCreateDeps& scd)
+        : mRenderDB(scd.renderDB),
+          mAudioSystem(scd.audioSystem),
+          mUISystem(scd.uiSystem) {}
+};
