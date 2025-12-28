87fb2d9007c5ce69b966b46e71c89c08f4963456
okuda <okuda2107@gmail.com>
2025-12-28 23:22:12 +0900

    wip: ActorsSystemのactorを分類しながら管理するようにした．未テスト

--- engine/include/game/object/ActorsSystem.h
+++ engine/include/game/object/ActorsSystem.h
@@ -1,12 +1,41 @@
 #pragma once
-#include <vector>
+#include <map>
+#include <string>
+#include <type_traits>
 
+#include "Actor.h"
 #include "base/ObjectsSystemBase.h"
+
+// 入力を処理するハンドラ
+struct ProcessInputHandlerBase {
+    virtual ~ProcessInputHandlerBase() = default;
+    virtual void operator()(class Actor* actor, const InputState& state) {
+        actor->ProcessInput(state);
+    }
+};
+
+// 更新を処理するハンドラ
+struct UpdateHandlerBase {
+    virtual ~UpdateHandlerBase() = default;
+    virtual void operator()(class Actor* actor, float deltatime) {
+        actor->Update(deltatime);
+    }
+};
+
+// それぞれのハンドラを束ねる構造体
+struct ActorExecutionPolicy {
+    ProcessInputHandlerBase* input;
+    UpdateHandlerBase* update;
+};
 
 // Actorの配列を保持し，それらのupdateを保証する
 class ActorsSystem : public ObjectsSystemBase<class InputState> {
-    std::vector<class Actor*> mActors;
-    std::vector<class Actor*> mPendingActors;
+    std::map<std::string, std::vector<class Actor*>> mActors;
+    std::map<std::string, std::vector<class Actor*>> mPendingActors;
+
+    // タグ別にハンドラを管理する
+    // todo: ActorExecutionPolicy* は smart pointer を使った方が良いらしい．
+    std::unordered_map<std::string, ActorExecutionPolicy*> mExecution;
 
     bool mUpdatingActors;
 
@@ -19,6 +48,27 @@ class ActorsSystem : public ObjectsSystemBase<class InputState> {
     void ProcessInput(const InputState& state);
     void UpdateObjects(float deltatime);
 
-    void AddActor(class Actor* actor);
-    void RemoveActor(class Actor* actor);
+    void AddActor(const std::string& tag, class Actor* actor);
+    void RemoveActor(const std::string& tag, class Actor* actor);
+
+    template <typename InputHandler, typename UpdateHandler>
+    void SetPolicy(const std::string& tag) {
+        static_assert(
+            std::is_base_of<ProcessInputHandlerBase, InputHandler>::value,
+            "InputHandler must derive from ProcessInputHandlerBase");
+        static_assert(std::is_base_of<UpdateHandlerBase, UpdateHandler>::value,
+                      "UpdateHandler must derive from UpdateHandlerBase");
+
+        auto iter = mExecution.find(tag);
+        if (iter != mExecution.end()) {
+            delete iter->second->input;
+            delete iter->second->update;
+            delete iter->second;
+            mExecution.erase(iter);
+        }
+        ActorExecutionPolicy* p = new ActorExecutionPolicy();
+        p->input = new InputHandler();
+        p->update = new UpdateHandler();
+        mExecution.emplace(tag, p);
+    }
 };
--- engine/src/game/object/ActorsSystem.cpp
+++ engine/src/game/object/ActorsSystem.cpp
@@ -7,33 +7,57 @@
 
 ActorsSystem::ActorsSystem() : mUpdatingActors(false) {}
 
-ActorsSystem::~ActorsSystem() { UnloadObjects(); }
+ActorsSystem::~ActorsSystem() {
+    UnloadObjects();
+    for (auto& [key, exe] : mExecution) {
+        delete exe->input;
+        delete exe->update;
+        delete exe;
+    }
+    mExecution.clear();
+}
 
 void ActorsSystem::ProcessInput(const InputState& state) {
     mUpdatingActors = true;
-    for (Actor* actor : mActors) {
-        actor->ProcessInput(state);
+    for (auto [key, actors] : mActors) {
+        for (Actor* actor : actors) {
+            auto iter = mExecution.find(key);
+            if (iter != mExecution.end())
+                (*iter->second->input)(actor, state);
+            else
+                actor->ProcessInput(state);
+        }
     }
     mUpdatingActors = false;
 }
 
 void ActorsSystem::UpdateObjects(float deltatime) {
     mUpdatingActors = true;
-    for (auto actor : mActors) {
-        actor->Update(deltatime);
+    for (auto [key, actors] : mActors) {
+        for (Actor* actor : actors) {
+            auto iter = mExecution.find(key);
+            if (iter != mExecution.end())
+                (*iter->second->update)(actor, deltatime);
+            else
+                actor->Update(deltatime);
+        }
     }
     mUpdatingActors = false;
 
-    for (auto pending : mPendingActors) {
-        pending->ComputeWorldTransform();
-        mActors.emplace_back(pending);
+    for (auto [key, pendings] : mPendingActors) {
+        for (Actor* pending : pendings) {
+            pending->ComputeWorldTransform();
+            mActors[key].emplace_back(pending);
+        }
     }
     mPendingActors.clear();
 
     std::vector<class Actor*> deadActors;
-    for (auto actor : mActors) {
-        if (actor->GetState() == Actor::State::EDead) {
-            deadActors.emplace_back(actor);
+    for (auto [key, actors] : mActors) {
+        for (auto actor : actors) {
+            if (actor->GetState() == Actor::State::EDead) {
+                deadActors.emplace_back(actor);
+            }
         }
     }
     for (auto actor : deadActors) {
@@ -42,26 +66,47 @@ void ActorsSystem::UpdateObjects(float deltatime) {
 }
 
 void ActorsSystem::UnloadObjects() {
-    while (!mActors.empty()) delete mActors.back();
+    for (auto [key, actors] : mActors)
+        while (!actors.empty()) delete actors.back();
 }
 
-void ActorsSystem::AddActor(Actor* actor) {
+void ActorsSystem::AddActor(const std::string& tag, Actor* actor) {
     if (mUpdatingActors) {
-        mPendingActors.push_back(actor);
+        auto iter = mPendingActors.find(tag);
+        if (iter != mPendingActors.end())
+            iter->second.push_back(actor);
+        else {
+            mPendingActors[tag] = std::vector<Actor*>();
+            mPendingActors[tag].push_back(actor);
+        }
     } else {
-        mActors.push_back(actor);
+        auto iter = mActors.find(tag);
+        if (iter != mActors.end())
+            iter->second.push_back(actor);
+        else {
+            mActors[tag] = std::vector<Actor*>();
+            mActors[tag].push_back(actor);
+        }
     }
 }
 
-void ActorsSystem::RemoveActor(Actor* actor) {
-    auto iter = find(mPendingActors.begin(), mPendingActors.end(), actor);
-    if (iter != mPendingActors.end()) {
-        std::iter_swap(iter, mPendingActors.end() - 1);
-        mPendingActors.pop_back();
+void ActorsSystem::RemoveActor(const std::string& tag, Actor* actor) {
+    auto mapIter = mPendingActors.find(tag);
+    if (mapIter != mPendingActors.end()) {
+        auto iter = find(mapIter->second.begin(), mapIter->second.end(), actor);
+        if (iter != mapIter->second.end()) {
+            std::iter_swap(iter, mapIter->second.end() - 1);
+            mapIter->second.pop_back();
+            return;
+        }
     }
-    iter = find(mActors.begin(), mActors.end(), actor);
-    if (iter != mActors.end()) {
-        std::iter_swap(iter, mActors.end() - 1);
-        mActors.pop_back();
+    mapIter = mActors.find(tag);
+    if (mapIter != mActors.end()) {
+        auto iter = find(mapIter->second.begin(), mapIter->second.end(), actor);
+        if (iter != mapIter->second.end()) {
+            std::iter_swap(iter, mapIter->second.end() - 1);
+            mapIter->second.pop_back();
+            return;
+        }
     }
 }
